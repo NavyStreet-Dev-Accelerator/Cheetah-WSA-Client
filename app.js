3e71bdc2c1c16197cff4a32175385e89beb8fbc3
Moath <moath.dlaii@gmail.com>
2020-08-29 16:23:06 -0700

    selected inputs and result div from html in js file

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 
+const submitBtn = document.getElementById('submitBtn');
+const url = document.getElementById('url');
+const word = document.getElementById('word');
+const result = document.getElementById('result')
+
